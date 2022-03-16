5c1a6f31bb21456f4c580e8a5b16332e77ddb32b
Douglas C Machado <douglasclaudino10.dc@gmail.com>
2022-03-16 00:32:43 -0300

    feature user pode escolher o valor

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+const min = Number(prompt("Escolha o número mínimo"))
+const max = Number(prompt("Escolha o número máximo"))
+
+console.log(min)
 function randomize(){
-  const random = Math.random() * (100000 - 1) + 1;
+  const random = Math.random() * (max - min) + min;
   const number = document.getElementById("random").innerHTML = Math.floor(random);
 }
 
